31184bd1fe8780868a61d8726ddff2094a7f77ed
deadbird11 <eddiejr@hatfieldfamily.com>
2020-02-19 16:48:14 -0500

    added minor TensorBoard support, will help this later. Also fixed wasserstein loss for disc and changed some model details, still not quite working

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from tensorflow.keras.datasets import mnist
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import time
+import datetime
 
 noise_dim = 100
 BUFFER_SIZE = 60000
 BATCH_SIZE = 64
 EPOCHS = 50
 display_interval = 2
+num_display_pics = 16
 
 (train_images, train_labels), (_, _) = mnist.load_data()
 
@@ -17,27 +20,32 @@ train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('fl
 train_images = (train_images - 127.5) / 127.5
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+
+# Define our metrics
+disc_loss_log = tf.keras.metrics.Mean('disc_loss', dtype=tf.float32)
+
 def make_generator_model():
     model = tf.keras.Sequential()
 
-    model.add(layers.Dense(7*7*256, input_dim=noise_dim))
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(noise_dim,)))
     model.add(layers.Reshape((7, 7, 256)))
 
     # CONV 1
-    model.add(layers.Conv2DTranspose(128, kernel_size=3, strides=2, padding='same'))
+    model.add(layers.Conv2DTranspose(128, kernel_size=3, strides=2, padding='same', use_bias=False))
     # maybe add axis=3?
     model.add(layers.BatchNormalization())
     model.add(layers.Activation('relu'))
 
     # CONV 2
-    model.add(layers.Conv2DTranspose(64, kernel_size=3, strides=1, padding='same'))
+    model.add(layers.Conv2DTranspose(64, kernel_size=3, strides=1, padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.Activation('relu'))
 
     # CONV 3
-    model.add(layers.Conv2DTranspose(1, kernel_size=2, strides=2))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Activation('tanh'))
+    model.add(layers.Conv2DTranspose(1, kernel_size=2, strides=2, use_bias=False, activation='tanh'))
 
     return model
 
@@ -60,14 +68,14 @@ def make_discriminator_model():
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=0.2))
 
-    model.add(layers.Reshape((7*7*256,)))
+    model.add(layers.Flatten())
     model.add(layers.Dense(1))
     model.add(layers.LeakyReLU(alpha=0.2))
 
     return model
 
 def discriminator_loss(real_pred, fake_pred):
-    return tf.reduce_mean(real_pred) - tf.reduce_mean(fake_pred)
+    return tf.reduce_mean(fake_pred) - tf.reduce_mean(real_pred)
 
 def generator_loss(fake_pred):
     return -tf.reduce_mean(fake_pred)
@@ -101,23 +109,30 @@ def train_step(images):
     gen_grad = gen_tape.gradient(gen_loss, generator.trainable_variables)
     disc_grad = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
+    disc_loss_log(disc_loss)
+
     gen_optimizer.apply_gradients(zip(gen_grad, generator.trainable_variables))
     disc_optimizer.apply_gradients(zip(disc_grad, discriminator.trainable_variables))
 
 def train(dataset, epochs):
-    seed = tf.random.normal([BATCH_SIZE, noise_dim])
+    seed = tf.random.normal([num_display_pics, noise_dim])
 
     for epoch in range(epochs):
+        start = time.time()
         for batch in dataset:
             train_step(batch)
-
-        print("On epoch ", epoch)
+        with train_summary_writer.as_default():
+            tf.summary.scalar('disc_loss', disc_loss_log.result(), step=epoch)
+        
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
         if (epoch + 1) % display_interval == 0:
             generate_and_save_images(generator, epoch, seed)
 
         if (epoch + 1) % 15 == 0:
             checkpoint.save(file_prefix = checkpoint_prefix)
+        
+        disc_loss_log.reset_states()
 
 
 def generate_and_save_images(model, epoch, test_input):
@@ -132,7 +147,7 @@ def generate_and_save_images(model, epoch, test_input):
       plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
       plt.axis('off')
 
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
+  plt.savefig('progress_images/image_at_epoch_{:04d}.png'.format(epoch))
+  plt.show(block=False)
 
 train(train_dataset, EPOCHS)
